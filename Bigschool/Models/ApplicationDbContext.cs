f21a04bd5e531fca336771fdcaa299c95a1e3a35
PCL9 <PCL9@S0903148>
2021-04-09 10:42:01 +0700

    Add project files.

---
+++ Bigschool/Models/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Bigschool.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Bigschool.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public DbSet<Course> Course { set; get; }
+        public DbSet<Category> Category { set; get; }
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
---
+++ Bigschool/ViewModels/CourseViewModel.cs
@@ -0,0 +1,32 @@
+﻿using Bigschool.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+
+namespace Bigschool.ViewModels
+{
+    public class CourseViewModel
+    {
+        [Required]
+        public string Place { get; set; }
+        [Required]
+        [FutureDate]
+        public string Date { get; set; }
+        [Required]
+        [ValidTime]
+        public string Time { get; set; }
+        [Required]
+        public byte Category { get; set; }
+        public IEnumerable<Category> Categories { set; get; }
+        public DateTime GetDateTime()
+        {
+            string D = Date;
+            string S = Time;
+            return DateTime.ParseExact(D, "dd/M/yyyy",
+                CultureInfo.InvariantCulture);
+        }
+    }
+}
